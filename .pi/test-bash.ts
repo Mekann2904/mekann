fe1755abe3b9a1952909bc97385121eaca4fecce
Matsumoto Kotaro <e235737@eve.u-ryukyu.ac.jp>
2026-02-15 19:41:44 +0900

    add

--- .pi/test-bash.ts
+++
@@ -1,8 +0,0 @@
-import { readdirSync } from "fs";
-import { join } from "path";
-
-const extDir = join(__dirname, "extensions");
-const files = readdirSync(extDir);
-
-console.log("Extensions in .pi/extensions/:");
-files.forEach(f => console.log(f));
